399647a07351aa3b9efab048428b6896cccf6a82
EmojiPlay88 <emojiplay105@gmail.com>
2024-12-03 21:25:03 +0100

    Separated the module

---
+++ steam-web-app/src/ReiviewSubmit.js
@@ -0,0 +1,24 @@
+import Button from "react-bootstrap/Button";
+import React, {useState} from "react";
+
+export default function ReviewSubmit (props) {
+    const [review, setReview] = useState("")
+    const handleSubmit = async (event) => {
+        await fetch("http://localhost:8000/gameinfo", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({"name": props.gameTitle})
+        });
+        const response = await fetch("http://localhost:8000/steamreview", {
+            method:"GET"
+        });
+        const reviewJson = await response.json();
+        setReview(reviewJson.data);
+        props.onClick(review);
+    }
+    return(
+        <div>
+            <Button onClick={handleSubmit}>Get your random review</Button>
+        </div>
+    );
+}
