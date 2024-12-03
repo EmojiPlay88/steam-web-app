9003ec2d6287814e8d79d5f1851980fa266dbc3e
EmojiPlay88 <emojiplay105@gmail.com>
2024-12-03 17:17:51 +0100

    Merged Get_review into Get_games

--- steam-web-app/src/Get_games.js
+++ steam-web-app/src/Get_games.js
@@ -1,10 +1,18 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useState, createContext, useContext} from "react";
 import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
-const GamesContext = React.createContext({
+const GamesContext = createContext({
     games: [], fetchGames: () => {}
 })
 
+const InputValueContxt = createContext({
+    inputValue: "", setInputValue: () => {}
+});
+
+const ReviewContext = createContext({
+    review: {}, setReview: () => {}
+})
 export default function Get_games(){
     const [games, setGames] = useState([])
     const [inputValue, setInputValue] = useState("");
@@ -34,12 +42,12 @@ export default function Get_games(){
             }
 
             // Как только собрали 25 уникальных игр — выходим из цикла
-            if (uniqueGames.length === 30) break;
+            if (uniqueGames.length === 100) break;
         }
 
         // Если уникальных игр меньше 25, добавляем оставшиеся из исходного списка
         for (const game of games) {
-            if (uniqueGames.length === 30) break;
+            if (uniqueGames.length === 100) break;
             if (!seenNames.has(game.name.toLowerCase())) {
                 uniqueGames.push(game);
                 seenNames.add(game.name.toLowerCase());
@@ -50,11 +58,38 @@ export default function Get_games(){
     };
     return(
         <GamesContext.Provider value={{games, fetchGames}}>
-            <Form.Select>
-                {filteredGames().map((game) => (
-                    <option key={game.name} value={game.name} />
-                ))}
-            </Form.Select>
+            <InputValueContxt.Provider value={{inputValue, setInputValue}}>
+                <Form.Select>
+                    {filteredGames().map((game) => (
+                        <option key={game.name} value={game.name}>
+                            {game.name}
+                        </option>
+                    ))}
+                </Form.Select>
+            </InputValueContxt.Provider>
         </GamesContext.Provider>
     )
+}
+
+export function Get_review(){
+    const { inputValue } = useContext(InputValueContxt);
+    const [review, setReview] = useState({})
+    const handleSubmit = async (event) => {
+        await fetch("http://localhost:8000/gameinfo", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({"name": inputValue})
+        });
+        const response = await fetch("http://localhost:8000/steamreview", {
+            method:"GET"
+        });
+        const reviewJson = await response.json;
+        setReview(reviewJson.data);
+        return review;
+    }
+    return(
+        <ReviewContext.Provider value={{review, setReview}}>
+            <Button onClick={handleSubmit}>Get your random review</Button>
+        </ReviewContext.Provider>
+    );
 }
