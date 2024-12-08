f4888f0df33d9ebb0f2bed241424e00ae6ed2afe
EmojiPlay88 <emojiplay105@gmail.com>
2024-12-08 20:43:07 +0100

    FINALLY GOT THIS STUFF WORKING

--- steam-web-app/src/ReiviewSubmit.js
+++ steam-web-app/src/ReiviewSubmit.js
@@ -1,20 +1,14 @@
 import Button from "react-bootstrap/Button";
-import React, {useState} from "react";
+import React from "react";
 import axios from "axios";
 
 export default function ReviewSubmit (props) {
-    const [review, setReview] = useState("")
     const handleSubmit = async (event) => {
-        console.log(props.gameTitle);
-        await axios.post("http://localhost:8000/gameinfo",
-            JSON.stringify({"name": props.gameTitle}),{
-            headers: {
-                "Content-Type": "application/json"
-            }
+        axios.get(`http://localhost:8000/steamreview?gameId=${props.game.id}`)
+            .then(response => {
+                console.log(response.data.data);
+                props.onClick(response.data.data);
         });
-        const response = await axios.get("http://localhost:8000/steamreview");
-        setReview(response.data.data);
-        props.onClick(review);
     }
     return(
         <div>
